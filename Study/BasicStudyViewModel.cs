7a7d9866cc675ab47d01f371f90cc4ab7809385f
Brent George <brentdgeorge1@gmail.com>
2020-07-27 17:48:01 -0600

    Basic study view model finished

---
+++ Study/BasicStudyViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using GalaSoft.MvvmLight.Views;
+using StudySmarterFlashcards.Sets;
+using StudySmarterFlashcards.Utils;
+
+namespace StudySmarterFlashcards.Study
+{
+  public class BasicStudyViewModel : BaseViewModel
+  {
+    #region Fields
+    private static readonly Random prRandom = new Random();
+    #endregion
+    #region Constructors
+    public BasicStudyViewModel(INavigationService navigationService) : base(navigationService)
+    {
+      Messenger.Default.Register<CardSetModel>(this, cardSetModel => InitializeSetPage(cardSetModel));
+      NavigateHomeCommand = new RelayCommand(NavigateHomeAction);
+      BackCommand = new RelayCommand(BackAction);
+      GoToNextFlashcardCommand = new RelayCommand(GoToNextFlashcard);
+      GoToPreviousFlashcardCommand = new RelayCommand(GoToPreviousFlashcard);
+    }
+    #endregion
+
+    #region Properties
+    public RelayCommand NavigateHomeCommand { get; private set; }
+    public RelayCommand BackCommand { get; private set; }
+    public RelayCommand GoToNextFlashcardCommand { get; private set; }
+    public RelayCommand GoToPreviousFlashcardCommand { get; private set; }
+    public CardSetModel FlashCardSet { get; private set; }
+    public int CurrentFlashcardIndex { get; private set; }
+    public IndividualCardModel CurrentFlashcard {
+      get {
+        return FlashCardSet.FlashcardCollection[CurrentFlashcardIndex];
+      }
+    }
+    public Stack<int> PreviousFlashcardIndexes { get; private set; }
+    public bool IsShuffleMode { get; set; } = true;
+    #endregion
+
+    #region Private Methods
+    private void NavigateHomeAction()
+    {
+      prNavigationService.NavigateTo("MainMenuPage");
+    }
+
+    private void InitializeSetPage(CardSetModel cardSetModel)
+    {
+      if (cardSetModel != null && cardSetModel.FlashcardCollection.Count > 0) {
+        FlashCardSet = cardSetModel;
+        CurrentFlashcardIndex = 0;
+        PreviousFlashcardIndexes = new Stack<int>();
+      } else {
+        throw new ArgumentNullException("Can't send null set to study page");
+      }
+      OnPropertyChanged("FlashCardSet");
+      OnPropertyChanged("CurrentFlashcardIndex");
+      OnPropertyChanged("CurrentFlashcard");
+    }
+
+    private void BackAction()
+    {
+      prNavigationService.GoBack();
+    }
+
+    private void GoToPreviousFlashcard()
+    {
+      if (PreviousFlashcardIndexes.Count > 0) {
+        CurrentFlashcardIndex = PreviousFlashcardIndexes.Pop();
+      }
+      OnPropertyChanged("CurrentFlashcardIndex");
+      OnPropertyChanged("CurrentFlashcard");
+    }
+
+    private void GoToNextFlashcard()
+    {
+      int currentIndex = CurrentFlashcardIndex; 
+      if (IsShuffleMode) {
+        int nextIndex = prRandom.Next(0, FlashCardSet.FlashcardCollection.Count - 1);
+        CurrentFlashcardIndex = currentIndex != nextIndex ? nextIndex : (nextIndex + 1) % FlashCardSet.FlashcardCollection.Count;
+      } else {
+        CurrentFlashcardIndex = (currentIndex + 1) % FlashCardSet.FlashcardCollection.Count;
+      }
+      PreviousFlashcardIndexes.Push(currentIndex);
+      OnPropertyChanged("CurrentFlashcardIndex");
+      OnPropertyChanged("CurrentFlashcard");
+    }
+    #endregion
+  }
+}
