5ccb51aa62e1290a6a760581dff39970ff2ce41c
Brent George <brentdgeorge1@gmail.com>
2020-08-01 15:25:58 -0600

    main menu page working

--- Utils/StringFormatConverter.cs
+++ Utils/StringFormatConverter.cs
@@ -3,44 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
 
 namespace StudySmarterFlashcards.Utils
 {
-  using System;
-  using System.Collections.Generic;
-  using System.Linq;
-  using System.Text;
-  using System.Threading.Tasks;
-  using Windows.UI.Xaml.Data;
-
-  namespace StudySmarterFlashcards.Utils
+  public class StringFormatConverter : IValueConverter
   {
-    public class StringFormatConverter : IValueConverter
+    public string StringFormat { get; set; }
+    public bool ConvertToLocalTime { get; set; }
+
+    public object Convert(object value, Type targetType, object parameter, string language)
     {
-      public string StringFormat { get; set; }
-      public bool ConvertToLocalTime { get; set; }
 
-      public object Convert(object value, Type targetType, object parameter, string language)
-      {
-
-        if (!String.IsNullOrEmpty(StringFormat)) {
-          if (value is DateTime && ((DateTime)value).Equals(DateTime.MinValue)) {
-            return String.Format(StringFormat, "Never");
-          } else {
-            if (ConvertToLocalTime) {
-              DateTime localDateTime = ((DateTime)value).ToLocalTime();
-              return String.Format(StringFormat, localDateTime);
-            }
-            return String.Format(StringFormat, value);
+      if (!String.IsNullOrEmpty(StringFormat)) {
+        if (value is DateTime && ((DateTime)value).Equals(DateTime.MinValue)) {
+          return String.Format(StringFormat, "Never");
+        } else {
+          if (ConvertToLocalTime) {
+            DateTime localDateTime = ((DateTime)value).ToLocalTime();
+            return String.Format(StringFormat, localDateTime);
           }
+          return String.Format(StringFormat, value);
         }
-        return value;
       }
+      return value;
+    }
 
-      public object ConvertBack(object value, Type targetType, object parameter, string language)
-      {
-        throw new NotImplementedException();
-      }
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+      throw new NotImplementedException();
     }
   }
 }
+
