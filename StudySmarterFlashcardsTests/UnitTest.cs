bf110f74caf5732bc6044ae907514832e34d26d1
Brent George <brentdgeorge1@gmail.com>
2020-08-13 10:35:40 -0600

    add test project

---
+++ StudySmarterFlashcardsTests/UnitTest.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StudySmarterFlashcardsTests
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+      Assert.AreEqual(0, 0);
+    }
+  }
+}
