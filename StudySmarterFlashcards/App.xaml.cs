ebd2ee1717cc57fbeadfe4cbef6ed862369e8af4
Brent George <brentdgeorge1@gmail.com>
2020-09-09 09:01:49 -0600

    reset to working build

--- StudySmarterFlashcards/App.xaml.cs
+++ StudySmarterFlashcards/App.xaml.cs
@@ -14,13 +14,16 @@ namespace StudySmarterFlashcards
   /// </summary>
   sealed partial class App : Application
   {
+    #region Security Fields
+    private static readonly string prSyncfusionLicence = "MzAzODM1QDMxMzgyZTMyMmUzMGZ0SU1lNjBraDVmSkhJamNBdW5rMkNRdVl4aWNnemZDVHluVkpsWnJEUUk9";
+    #endregion
     /// <summary>
     /// Initializes the singleton application object.  This is the first line of authored code
     /// executed, and as such is the logical equivalent of main() or WinMain().
     /// </summary>
     public App()
     {
-      Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzAzODM1QDMxMzgyZTMyMmUzMGZ0SU1lNjBraDVmSkhJamNBdW5rMkNRdVl4aWNnemZDVHluVkpsWnJEUUk9");
+      Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(prSyncfusionLicence);
       this.InitializeComponent();
       this.Suspending += OnSuspending;
     }
